97abc91f7577d78e1673181dd7885ca3e0478889
Enes Kantarci <enskntrc@gmail.com>
2024-09-01 15:02:01 +0300

    Icons Handled

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { remixDevTools } from "remix-development-tools";
+import { vitePlugin as remix } from "@remix-run/dev";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { iconsSpritesheet } from "vite-plugin-icons-spritesheet";
 
 export default defineConfig({
   plugins: [
@@ -16,6 +17,13 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    iconsSpritesheet({
+      inputDir: "./resources/svg-icons",
+      outputDir: "./public",
+      typesOutputFile: "./app/components/types/icon.ts",
+      withTypes: true,
+      fileName: "icons.svg",
+    }),
   ],
   server: {
     open: true,
