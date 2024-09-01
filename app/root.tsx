8416d5b094294362ca4f981d79884fcecb812f95
Enes Kantarci <enskntrc@gmail.com>
2024-09-01 15:50:46 +0300

    flat routes and remix toast has added

--- app/root.tsx
+++ app/root.tsx
@@ -1,23 +1,58 @@
+import { useEffect } from "react";
+import { LoaderFunctionArgs } from "@remix-run/node";
 import {
+  isRouteErrorResponse,
+  json,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useRouteError,
 } from "@remix-run/react";
-import "./tailwind.css";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+import { getToast } from "remix-toast";
+import { Toaster, toast as notify } from "sonner";
+
+import "./tailwind.css";
+import { Message } from "./components/feedback/error";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { toast, headers } = await getToast(request);
+  return json({ toast }, { headers });
+};
+
+export function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { toast } = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (toast?.type === "error") {
+      notify.error(toast.message);
+    }
+    if (toast?.type === "success") {
+      notify.success(toast.message);
+    }
+  }, [toast]);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
         <Meta />
         <Links />
       </head>
       <body>
         {children}
+        <Toaster />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -26,5 +61,61 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <RootLayout>
+      <Outlet />
+    </RootLayout>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  const status = isRouteErrorResponse(error)
+    ? error.status
+    : error instanceof Error
+    ? error.stack
+    : 500;
+  const statusText = isRouteErrorResponse(error)
+    ? error.statusText
+    : error instanceof Error
+    ? error.name
+    : "Something went wrong";
+  const message = isRouteErrorResponse(error)
+    ? error.data
+    : error instanceof Error
+    ? error.message
+    : "Unknown error";
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <RootLayout>
+        <Message
+          status={status}
+          statusText={statusText}
+          message={message}
+        />
+      </RootLayout>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <RootLayout>
+        <Message
+          status={status}
+          statusText={statusText}
+          message={message}
+        />
+      </RootLayout>
+    );
+  } else {
+    return (
+      <RootLayout>
+        <Message
+          status={status}
+          statusText={statusText}
+          message={message}
+        />
+      </RootLayout>
+    );
+  }
 }
